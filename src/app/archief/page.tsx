2dfc918539219aef31daac4109e23d3ff41abfef
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-02-19 09:56:33 +0100

    update

--- src/app/archief/page.tsx
+++ src/app/archief/page.tsx
@@ -1,12 +1,12 @@
 import { Metadata } from "next";
 
-import { SliceZone } from "@prismicio/react";
+import { PrismicImage, SliceZone } from "@prismicio/react";
 import * as prismic from "@prismicio/client";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 import Layout from "@/components/layout"
-
+import Link from "next/link";
 
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -28,11 +28,22 @@ export default async function Index() {
   const client = createClient();
   const home = await client.getByUID("page", "home");
   const navigation = await client.getByType("navigation");
+  const projects = await client.getAllByType('project');
 
   return (
     <div className="archief">
       <Layout navigation={navigation.results[0].data}>
         <h1 className="page-title">Archief</h1>
+        <div className="projects-grid">
+          {projects.map((item, i) => {
+            return(
+              <div key={`project${i}`} className="project-item">
+                <div className="image"><PrismicImage field={item.data.image}/></div>
+                <Link href={`/projects/${item.uid}`}>{item.data.title}</Link>
+              </div>
+            )
+          })}
+        </div>
         <SliceZone slices={home.data.slices} components={components} />
       </Layout>
     </div>
