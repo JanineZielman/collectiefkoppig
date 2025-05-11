b251b1859244bcdaa29113705a995e50b9cb0fb3
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-05-11 20:36:50 +0200

    image slider

--- src/slices/ImageSlider/index.tsx
+++ src/slices/ImageSlider/index.tsx
@@ -1,5 +1,9 @@
+'use client'
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import dynamic from "next/dynamic";
+import { PrismicNextImage } from "@prismicio/next";
+import styles from "./Slider.module.scss"
 
 /**
  * Props for `ImageSlider`.
@@ -10,13 +14,38 @@ export type ImageSliderProps = SliceComponentProps<Content.ImageSliderSlice>;
  * Component for "ImageSlider" Slices.
  */
 const ImageSlider = ({ slice }: ImageSliderProps): JSX.Element => {
+  const Slider = dynamic(() => import("react-slick"), { ssr: false });
+  console.log(slice.primary)
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          variableWidth: false,
+        }
+      }
+    ]
+  };
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={styles.slider}
     >
-      Placeholder component for image_slider (variation: {slice.variation})
-      Slices
+      <Slider {...settings}>
+        {slice.primary.images.map((item) => (
+          <PrismicNextImage field={item.image} />
+        ))}
+      </Slider>
     </section>
   );
 };
